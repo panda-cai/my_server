b93890a554b8230e9d404065e0af5f1ea2f94fb9
Caishengda <448456539@qq.com>
2019-10-10 20:31:15 +0800

    CART

--- routers/product.js
+++ routers/product.js
@@ -34,4 +34,39 @@ router.get("/productList",(req,res)=>{
   }
   
 })
+router.get("/detail",(req,res)=>{
+  var $pid=req.query.pid;
+  console.log($pid);
+  pool.query('SELECT * FROM product WHERE pid=?',[$pid],(err,result)=>{
+    if(err) throw err;
+    console.log(result[0].pid);
+    res.send(result);
+  })
+})
+
+router.get("/cart",(req,res)=>{
+  if(!req.session.uid){
+    res.send({code:-2,msg:'请登陆'});
+    return
+  }else{
+    var $uid=req.session.uid;
+  }
+  var $show_img=req.query.show_img;
+  var $pid=req.query.pid;
+  var $style=req.query.style;
+  var $spec=req.query.spec;
+  pool.query('SELECT c_id,count FROM cart WHERE pid=? AND uid=?',[$pid,$uid],(err,result)=>{
+    if(err) throw err;
+    if(result.length>0){
+      var $c_id=result[0].c_id;
+      var $count=result[0].count++;
+      pool.query('UPDATE cart SET count=? WHERE c_id=?',[$count,$c_id],(err,result)=>{
+        if(err) throw err;
+        res.send({ code:1, msg: "请登陆" });
+      });
+    }else{
+      pool.query('INSERT INTO cart VALUES(NULL,)')
+    }
+  })
+})
 module.exports=router;
